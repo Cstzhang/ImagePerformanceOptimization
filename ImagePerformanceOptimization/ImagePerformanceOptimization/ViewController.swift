eb5aaf115c8d101e574f0a35dd0baf5ca70202b4
Cstzhang <835066041@qq.com>
2017-11-02 15:28:33 +0800

    处理图片拉伸问题

--- ImagePerformanceOptimization/ImagePerformanceOptimization/ViewController.swift
+++ ImagePerformanceOptimization/ImagePerformanceOptimization/ViewController.swift
@@ -12,18 +12,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let iv  = UIImageView(frame: CGRect(x: 0, y: 0, width: 160, height: 160))
-        
+        let rect = CGRect(x: 0, y: 0, width: 160, height: 160)
+        let iv  = UIImageView(frame: rect)
         iv.center = view.center
-        
         view.addSubview(iv)
-        
-        iv.image = #imageLiteral(resourceName: "icon.png")
-        
-        
-        
+        let image = #imageLiteral(resourceName: "avatar_default_big.png")
+        iv.image = avatarImage(image: image, size: rect.size)
     }
    
+    // MARK: -图片拉伸处理
+    
+    /// 将原来的图像拉伸后返回新的图像
+    ///
+    /// - Parameters:
+    ///   - image: 图像
+    ///   - size: 尺寸
+    /// - Returns: 处理后的图像
+    func avatarImage(image:UIImage,size:CGSize) -> UIImage? {
+        // 1 上下文 在内存中开辟地址，与屏幕显示无关
+        let rect  = CGRect(origin: CGPoint(), size: size)
+        /*
+         1>size
+         2>不透明
+         3>屏幕分辨率 如果不指定，默认使用1.0的分辨率（图片质量不好）
+           指定0，会选择当前设备屏幕的分辨率
+         */
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        // 2 绘图
+        image.draw(in: rect)
+        // 3 取得新的图像
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        // 4 关闭上下文
+        UIGraphicsEndImageContext()
+        // 5 返回结果
+        
+        return result
+    }
+    
+    
+    
+    
 
 }
 
