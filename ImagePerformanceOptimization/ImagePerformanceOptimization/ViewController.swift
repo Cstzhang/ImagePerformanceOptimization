583f5464bed4576aaca1b23b700f1ebe5d239b96
Cstzhang <835066041@qq.com>
2017-11-02 17:14:41 +0800

    添加绘图

--- ImagePerformanceOptimization/ImagePerformanceOptimization/ViewController.swift
+++ ImagePerformanceOptimization/ImagePerformanceOptimization/ViewController.swift
@@ -17,9 +17,11 @@ class ViewController: UIViewController {
         iv.center = view.center
         view.addSubview(iv)
         let image = #imageLiteral(resourceName: "avatar_default_big.png")
-        iv.image = avatarImage(image: image, size: rect.size)
+        iv.image =  avatarImage(image: image, size: rect.size,backColor: view.backgroundColor)
     }
    
+    
+    
     // MARK: -图片拉伸处理
     
     /// 将原来的图像拉伸后返回新的图像
@@ -28,24 +30,36 @@ class ViewController: UIViewController {
     ///   - image: 图像
     ///   - size: 尺寸
     /// - Returns: 处理后的图像
-    func avatarImage(image:UIImage,size:CGSize) -> UIImage? {
+    func avatarImage(image:UIImage,size:CGSize,backColor:UIColor?) -> UIImage? {
         // 1 上下文 在内存中开辟地址，与屏幕显示无关
         let rect  = CGRect(origin: CGPoint(), size: size)
         /*
          1>size
-         2>不透明
+         2>不透明 (混合)
          3>屏幕分辨率 如果不指定，默认使用1.0的分辨率（图片质量不好）
            指定0，会选择当前设备屏幕的分辨率
          */
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
+        
+        // 圆角
+        // （0） 背景填充
+        backColor?.setFill()
+        UIRectFill(rect)
+        // （1） 原型路径
+        let path = UIBezierPath(ovalIn: rect)
+        // （2） 路径裁切
+        path.addClip()
         // 2 绘图
         image.draw(in: rect)
+        // 绘制内切原型
+        UIColor.darkGray.setStroke()
+        path.lineWidth = 2
+        path.stroke()
         // 3 取得新的图像
         let result = UIGraphicsGetImageFromCurrentImageContext()
         // 4 关闭上下文
         UIGraphicsEndImageContext()
         // 5 返回结果
-        
         return result
     }
     
